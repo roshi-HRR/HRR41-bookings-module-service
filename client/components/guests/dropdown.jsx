f647d7bbef576a45a5431ede7033466b26a80f60
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-10-18 01:22:24 -0700

    Got visual part of dropdown completed

--- client/components/guests/dropdown.jsx
+++ client/components/guests/dropdown.jsx
@@ -3,13 +3,36 @@ import Styles from './styles.js';
 
 class Dropdown extends Component {
   constructor(props){
-    super(props)
+    super(props);
+
+    this.state = {
+      adults: 1,
+      children: 0,
+      infants: 0
+    }
   }
 
   render() {
     return (
-      <div style = {Styles.mainDropDown}>
-
+      <div style = {{...Styles.mainDropDown, ...Styles.columnWrapper}}>
+        <div style={Styles.rowWrapper}>
+          <p style={Styles.guestText}>Adults</p>
+          <div style={Styles.circle}>+</div>
+          <p style={Styles.guestText}>{this.state.adults}</p>
+          <div style={Styles.circle}>-</div>
+        </div>
+        <div style={Styles.rowWrapper}>
+          <p style={Styles.guestText}>Children</p>
+          <div style={Styles.circle}>+</div>
+          <p style={Styles.guestText}>{this.state.children}</p>
+          <div style={Styles.circle}>-</div>
+        </div>
+        <div style={Styles.rowWrapper}>
+          <p style={Styles.guestText}>Infants</p>
+          <div style={Styles.circle}>+</div>
+          <p style={Styles.guestText}>{this.state.infants}</p>
+          <div style={Styles.circle}>-</div>
+        </div>
       </div>
     )
   }
--- client/components/guests/styles.js
+++ client/components/guests/styles.js
@@ -9,6 +9,37 @@ const guestStyles = {
     borderWidth: 'thin',
     marginLeft: '25px',
     zIndex: 3
+  },
+  columnWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  rowWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  guestText: {
+    fontSize: '16px',
+    fontWeight: 50,
+    fontFamily: 'sans-serif',
+    color: '#484848',
+    userSelect: 'none',
+    marginLeft: '10px'
+  },
+  circle: {
+    borderStyle: 'solid',
+    borderRadius: '50%',
+    height: '20px',
+    width: '20px',
+    textAlign: 'center',
+    fontSize: '16px',
+    fontWeight: 50,
+    fontFamily: 'sans-serif',
+    color: '#484848',
+    userSelect: 'none',
+    marginLeft: '10px'
   }
 }
 
