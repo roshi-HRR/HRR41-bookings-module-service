4082d30f1b3d49907e49b3528d25987c7796d52f
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-10-19 14:13:34 -0700

    Added on click function to booked dates

--- client/components/calendar/week.jsx
+++ client/components/calendar/week.jsx
@@ -63,7 +63,7 @@ class Week extends Component {
         return <td key={i} style={Styles.calendarTextGrey} >{date}</td>;
       }
       else if(this.isBetween(moment().month()+1+this.props.incrementMonth + '-' + date + '-' + moment().month(moment().month() + this.props.incrementMonth).format('YYYY'))){
-        return <td key={i} style={Styles.calendarTextRed} >{date}</td>;
+        return <td onClick={() => {this.props.getDate(date, moment().month()+1+this.props.incrementMonth,  moment().month(moment().month() + this.props.incrementMonth).format('YYYY'), this.props.calType)}} key={i} style={Styles.calendarTextRed} >{date}</td>;
       }
       else if(moment().date() < date && this.props.incrementMonth === 0){
         return  <td onClick={() => {this.props.getDate(date, moment().month()+1+this.props.incrementMonth,  moment().month(moment().month() + this.props.incrementMonth).format('YYYY'), this.props.calType)}} key={i} style={Styles.calendarText} >{date}</td>;
