a5633715cd0949f58b06c8708d15f00afd41b250
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-10-19 12:50:32 -0700

    Better format for conditionally rendering dates

--- client/components/calendar/week.jsx
+++ client/components/calendar/week.jsx
@@ -9,23 +9,28 @@ class Week extends Component {
   }
 
   render() {
-    let getDateBlack = (i, date) => {
-      return  <td onClick={() => {this.props.getDate(date, moment().month()+1+this.props.incrementMonth,  moment().month(moment().month() + this.props.incrementMonth).format('YYYY'), this.props.calType)}} key={i} style={Styles.calendarText} >{date}</td>
+    let renderDate = (date, i) => {
+      if(moment().date() > date && this.props.incrementMonth === 0){
+        return <td key={i} style={Styles.calendarTextGrey} >{date}</td>;
+      }
+      else if(moment().date() < date && this.props.incrementMonth === 0){
+        return  <td onClick={() => {this.props.getDate(date, moment().month()+1+this.props.incrementMonth,  moment().month(moment().month() + this.props.incrementMonth).format('YYYY'), this.props.calType)}} key={i} style={Styles.calendarText} >{date}</td>;
+      }
+      else if( this.props.incrementMonth < 0){
+        return <td key={i} style={Styles.calendarTextGrey} >{date}</td>;
+      }
+      else{
+        return  <td onClick={() => {this.props.getDate(date, moment().month()+1+this.props.incrementMonth,  moment().month(moment().month() + this.props.incrementMonth).format('YYYY'), this.props.calType)}} key={i} style={Styles.calendarText} >{date}</td>;
+      }
     }
 
-    let getDateGrey = (i, date) => {
-      return <td key={i} style={Styles.calendarTextGrey} >{date}</td>;
+    let isDate = (day, month, year) => {
+
     }
 
     return (
       <tr>
-        {this.props.dates.map((date, i) => moment().date() > date && this.props.incrementMonth === 0 ?
-         getDateGrey(i, date) :
-          moment().date() < date && this.props.incrementMonth === 0 ?
-           getDateBlack(i, date) :
-            this.props.incrementMonth < 0 ?
-              getDateGrey(i, date) :
-              getDateBlack(i, date))}
+        {this.props.dates.map((date, i) => renderDate(date, i))}
       </tr>
     )
   }
