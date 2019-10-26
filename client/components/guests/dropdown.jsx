5ce632c2d1104c315920003b2d9e65640a3088d2
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-10-25 18:08:10 -0700

    Changed some positioning with guest dropdown component

--- client/components/guests/dropdown.jsx
+++ client/components/guests/dropdown.jsx
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../css/guests.css';
 
 class Dropdown extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       adults: 1,
       children: 0,
-      infants: 0
-    }
+      infants: 0,
+    };
   }
 
   render() {
@@ -17,24 +17,24 @@ class Dropdown extends Component {
       <div className="main-drop-down column-wrapper">
         <div className="row-wrapper">
           <p className="guest-text">Adults</p>
-          <div onClick={this.props.decreaseAdults} className="circle"><span className="number">-</span></div>
+          <div onClick={this.props.decreaseAdults} className="circle" style={{left:'25px'}}><span className="number">-</span></div>
           <p className="guest-text">{this.props.adults}</p>
-          <div onClick={this.props.increaseAdults} className="circle"><span className="number">+</span></div>
+          <div onClick={this.props.increaseAdults} className="circle" style={{right: '15px'}}><span className="number">+</span></div>
         </div>
         <div className="row-wrapper">
           <p className="guest-text">Children</p>
-          <div onClick={this.props.decreaseChildren} className="circle"><span className="number">-</span></div>
+          <div onClick={this.props.decreaseChildren} className="circle" style={{left:'18px'}}><span className="number">-</span></div>
           <p className="guest-text">{this.props.children}</p>
-          <div onClick={this.props.increaseChildren} className="circle"><span className="number">+</span></div>
+          <div onClick={this.props.increaseChildren} className="circle" style={{right: '15px'}}><span className="number">+</span></div>
         </div>
         <div className="row-wrapper">
           <p className="guest-text">Infants</p>
-          <div onClick={this.props.decreaseInfants} className="circle"><span className="number">-</span></div>
+          <div onClick={this.props.decreaseInfants} className="circle" style={{left:'25px'}}><span className="number">-</span></div>
           <p className="guest-text">{this.props.infants}</p>
-          <div onClick={this.props.increaseInfants} className="circle"><span className="number">+</span></div>
+          <div onClick={this.props.increaseInfants} className="circle" style={{right: '15px'}}><span className="number">+</span></div>
         </div>
       </div>
-    )
+    );
   }
 }
 
